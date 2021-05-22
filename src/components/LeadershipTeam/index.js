265cf275f17c2b9e9b6a85231d4acc20e76dbe8d
Erick-Bravo <erickbrvo@gmail.com>
2021-05-22 00:08:45 -0700

    Leadership statement aligned correctly

--- src/components/LeadershipTeam/index.js
+++ src/components/LeadershipTeam/index.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./index.css";
 
+const LeadCard = ({lead}) => {
+    return (
+        <section>
+
+        </section>
+    );
+};
+
+
+
 const LeadershipTeam = () => {
+
+    const [ leaders, setLeaders ] = useState(null)
+
+    useEffect(async () => {
+        const response = await fetch("https://beamtech.github.io/marketing-kata-html/leadership-team.json");
+        const data = await response.json();
+        // await console.log(data)
+        // await console.log(response)
+        await setLeaders(data);
+    }, [])
+
+    useEffect(() => {
+
+    }, [leaders])
+
+    console.log(`AHHHHHHHHHHH ${leaders}`)
+
     return (
         <section className="leadership-container">
             <h2>Our Leadership Team</h2>
